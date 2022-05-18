c834d16311fa66d1e7d7a65889db9eed87cc0bba
Thomas-Schrder <101568516+Thomas-Schrder@users.noreply.github.com>
2022-05-18 08:38:36 +0200

    Add files via upload

---
+++ Semester_App/Semester_App/app/src/main/java/com/example/semester_app/data/pojo/MealsResponse.kt
@@ -0,0 +1,7 @@
+package com.example.semester_app.data.pojo
+
+import com.example.semester_app.data.pojo.Meal
+
+data class MealsResponse(
+    val meals: List<Meal>
+)
---
+++ Semester_App/Semester_App/app/src/main/java/com/example/semester_app/data/pojo/RandomMealResponse.kt
@@ -0,0 +1,7 @@
+package com.example.semester_app.data.pojo
+
+import com.example.semester_app.data.pojo.MealDetail
+
+data class RandomMealResponse(
+    val meals: List<MealDetail>
+)
